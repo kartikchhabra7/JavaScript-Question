55fbde5ad6874e148c09bc2085b01192a1c2aec2
Kartik Chhabra <129652398+kartikchhabra7@users.noreply.github.com>
2023-04-09 15:53:02 +0530

    Initial commit

---
+++ ReverseArray.js
@@ -0,0 +1,33 @@
+/*            INPUT : [1,2,3,4,5] 
+              OUTPUT: [5,4,3,2,1] */
+
+      
+      /*------------------ReduceRight Method---------------------*/
+      
+      let arr = [1, 2, 3, 4, 5];
+      let newArr = arr.reduceRight((accm, elm) => {
+        return accm + " " + elm;
+      }, []);
+      console.log(newArr);
+
+      
+      /*------------------Reverse Method---------------------*/
+
+      let a = [1, 2, 3, 4, 5];
+      let b = a.reverse();
+      console.log(a);
+
+      
+      /*------------------Slice Method---------------------*/
+      
+        test = function () {
+        arr = [1, 2, 3, 4, 5];
+        arr
+          .slice()
+          .reverse()
+          .forEach((val) => {
+            console.log(val);
+          });
+      };
+
+      test();
